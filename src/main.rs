1447cffb230ab3983a3e5839ee340553cb13ee65
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-02-02 11:07:08 +0100

    0.1.2

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,31 @@ fn get_pwd_as_string() -> String {
     }
 }
 
-fn tilde_home_directory(path: String) -> Option<String> {
+fn tilde_home_directory(path: &str) -> Option<String> {
     Some(String::from("~") + path.strip_prefix(&env::var("HOME").ok()?)?)
 }
 
 fn tilde_formatted_pwd() -> String {
-    match tilde_home_directory(get_pwd_as_string()) {
+    match tilde_home_directory(&get_pwd_as_string()) {
         Some(p) => p,
         None => get_pwd_as_string(),
     }
 }
 
-fn shorten_workspace_names(path: String) -> String {
+fn shorten_workspace_names(path: &str) -> String {
     path.split('/')
         .enumerate()
-        .map_while(|(i, s)| (i != path.split('/').count() - 1).then(|| s))
+        .map_while(|(i, s)| (i != path.split('/').count() - 1).then_some(s))
         .map(|s| match s.get(..2) {
             Some(v) => v,
             None => s,
         })
-        .fold(String::from(""), |acc, cur| acc + cur + "/")
+        .fold(String::new(), |acc, cur| acc + cur + "/")
         + path.split('/').last().unwrap()
 }
 
 fn shorten_tilde_formatted_pwd() -> String {
-    shorten_workspace_names(tilde_formatted_pwd())
+    shorten_workspace_names(&tilde_formatted_pwd())
 }
 
 fn main() {
